8933f8d87914abbc7add1e1b379efd1f5b542217
Tomek <tomaszhofdun@gmail.com>
2019-04-10 22:53:43 +0200

    start working on this section

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,22 +2,22 @@ var gulp = require("gulp"),
   watch = require("gulp-watch"),
   browserSync = require("browser-sync").create();
 
-gulp.task("watch", function() {
+gulp.task("watch", function () {
   browserSync.init({
-    notify: false,
+    notify: true,
     server: {
       baseDir: "app"
     }
   });
 
-  watch("app/index.html", function() {
+  watch("app/index.html", function () {
     browserSync.reload();
   });
-  watch("app/assets/styles/**/*.css", function() {
+  watch("app/assets/styles/**/*.css", function () {
     gulp.start("cssInject");
   });
 });
 
-gulp.task("cssInject", ["css"], function() {
+gulp.task("cssInject", ["css"], function () {
   return gulp.src("app/temp/styles/styles.css").pipe(browserSync.stream());
 });
