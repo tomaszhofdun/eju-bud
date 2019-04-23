203a755f8697c4fb4fc9e01a94682bf2ea46e4ce
Tomek <tomaszhofdun@gmail.com>
2019-04-23 21:18:33 +0200

    finished 3 items which reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,13 @@ import MobileMenu from "./modules/_mobileMenu";
 import CurrentDate from "./modules/_currentDate";
 import GallerySlider from "./modules/_gallerySlider";
 import LtrScale from "./modules/_ltrScale";
+import RevealOnScroll from "./modules/_revealOnScroll";
+import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
 var currentDate = new CurrentDate();
 var gallerySlider = new GallerySlider();
 var ltrScale = new LtrScale();
+new RevealOnScroll($(".huge-icon-section_div"), '70%');
+new RevealOnScroll($(".gallery-thumbnails__title"), '70%');
+new RevealOnScroll($(".form__title"), '80%');
