7f4570b51abc5bd32ad38785eb9e7cf5cdbcebd7
Tomek <tomaszhofdun@gmail.com>
2019-04-28 16:15:10 +0200

    finished full-automatic svg compile

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,10 +1,13 @@
 var gulp = require("gulp"),
   rename = require("gulp-rename"),
-  svg = require("gulp-svg-sprite");
+  svg = require("gulp-svg-sprite"),
+  del = require("del");
 
+//   plik konfiguracyjny sprite ze scieżką do template
 config = {
   mode: {
     css: {
+      sprite: "sprite.svg",
       render: {
         css: {
           template: "gulp/templates/sprite.css"
@@ -14,16 +17,37 @@ config = {
   }
 };
 
-gulp.task("createSprite", function() {
+gulp.task("deleteOldSpriteCss", function() {
+  return del(["app/temp/sprite", "app/assets/images/icons/svg-compiled"]);
+});
+
+// sprite tworzy css-a z backgroundem i pozycją ikon oraz tworzy plik .svg ze wszystkimi ikonami
+gulp.task("createSprite", ["deleteOldSpriteCss"], function() {
   return gulp
     .src("app/assets/images/icons/**/*.svg")
     .pipe(svg(config))
     .pipe(gulp.dest("app/temp/sprite/"));
 });
 
-gulp.task("copySpriteCss", function() {
+// kopiujemy wygenerowany plik css tam gdzie mamy wszystkie style
+gulp.task("copySpriteCss", ["createSprite"], function() {
   return gulp
     .src("app/temp/sprite/css/*.css")
     .pipe(rename("_sprite.css"))
     .pipe(gulp.dest("app/assets/styles/modules/"));
 });
+gulp.task("copySpriteSvg", ["createSprite"], function() {
+  return gulp
+    .src("app/temp/sprite/css/**/*.svg")
+    .pipe(gulp.dest("app/assets/images/icons/svg-compiled/"));
+});
+gulp.task("endCleaning", ["copySpriteCss", "copySpriteSvg"], function() {
+  return del("app/temp/sprite");
+});
+
+gulp.task("icons", [
+  "deleteOldSpriteCss",
+  "copySpriteCss",
+  "copySpriteSvg",
+  "endCleaning"
+]);
