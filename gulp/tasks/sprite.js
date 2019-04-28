4f571cc505c19423d8755c88d057c3f16759c59b
Tomek <tomaszhofdun@gmail.com>
2019-04-28 12:20:27 +0200

    replaced all icons on .svg sprite background

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,7 +1,29 @@
 var gulp = require("gulp"),
-    svg = require("gulp-svg-sprite");
+  rename = require("gulp-rename"),
+  svg = require("gulp-svg-sprite");
 
-gulp.task("createSprite", function () {
-    return src("app/assets/images/icons/**/*.svg")
-        .pipe(gulp.dest("app/temp/sprite/"));
-})
+config = {
+  mode: {
+    css: {
+      render: {
+        css: {
+          template: "gulp/templates/sprite.css"
+        }
+      }
+    }
+  }
+};
+
+gulp.task("createSprite", function() {
+  return gulp
+    .src("app/assets/images/icons/**/*.svg")
+    .pipe(svg(config))
+    .pipe(gulp.dest("app/temp/sprite/"));
+});
+
+gulp.task("copySpriteCss", function() {
+  return gulp
+    .src("app/temp/sprite/css/*.css")
+    .pipe(rename("_sprite.css"))
+    .pipe(gulp.dest("app/assets/styles/modules/"));
+});
